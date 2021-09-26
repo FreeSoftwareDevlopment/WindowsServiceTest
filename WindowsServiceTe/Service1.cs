0d36c32051000896ca34a7d2c475f9de0502982c
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2021-09-26 18:00:50 +0200

    Projektdateien hinzufügen.

---
+++ WindowsServiceTe/Service1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceTe
+{
+    public partial class WindowsServiceTe : ServiceBase
+    {
+        public WindowsServiceTe()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.IO.File.WriteAllText("C:\\hi.txt", "helloworld");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
