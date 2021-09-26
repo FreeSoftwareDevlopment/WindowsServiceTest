0b80a90e453de90e0892a8c93f72f727bf029a0c
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2021-09-26 18:29:07 +0200

    UPD

--- WindowsServiceTe/Program.cs
+++ WindowsServiceTe/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +14,30 @@ namespace WindowsServiceTe
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            if (System.Environment.UserInteractive)
             {
+                string parameter = string.Concat(args);
+                switch (parameter)
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+                        break;
+                    case "--uninstall":
+                        ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+                        break;
+                }
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
                 new WindowsServiceTe()
-            };
-            ServiceBase.Run(ServicesToRun);
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
         }
     }
 }
--- WindowsServiceTe/Service1.cs
+++ WindowsServiceTe/Service1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WindowsServiceTe
 {
@@ -19,7 +20,7 @@ namespace WindowsServiceTe
 
         protected override void OnStart(string[] args)
         {
-            System.IO.File.WriteAllText("C:\\hi.txt", "helloworld");
+            File.WriteAllText("C:\\hi.txt", "helloworld");
         }
 
         protected override void OnStop()
